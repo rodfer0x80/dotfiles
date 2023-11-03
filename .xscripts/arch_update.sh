b0451cd8632188ee784755cc5ed8fff9a80e32c7
rodfer0x80 <rodrigolf080@gmail.com>
2023-11-03 01:02:07 +0000

    rawdawg

--- .xscripts/arch_update.sh
+++ .xscripts/arch_update.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-# sync
+# refresh
 reset
 sync
 # update pacmandb
-yes | pkgfile -u || exit 1
-yes | pacman -Fyy || exit 2 
-yes | pacman-db-upgrade || exit 3 
-yes | pacman -Scc || exit 4 
-# update pacman keyring
-yes | pacman -Sy archlinux-keyring || exit 5 
-# update pacman
-yes | pacman -Syyu || exit 6
+yes | sudo pkgfile -u || exit 1
+yes | sudo pacman -Fyy || exit 2 
+yes | sudo pacman-db-upgrade || exit 3 
+yes | sudo pacman -Scc || exit 4 
+# update pacman keyring first
+# only then update other packages
+yes | sudo pacman -Syyu archlinux-keyring || exit 5 
 # update AUR
-yes | yay -Syua || exit 7 
-#yes |  pacman -Rscn $(yay -Qtdq)
-yes | yay -Yc || exit 8 
-# update python pip
-yes | python -m pip install --upgrade pip || exit 9 
+yes | sudo yay -Syua || exit 7 
+# yes |  pacman -Rscn $(yay -Qtdq)
+yes | sudo yay -Yc || exit 8 
+# update system python3 pip
+yes | sudo python3 -m pip install --upgrade pip || exit 9 
+# update user python3-venv pip
 yes | py -m pip install --upgrade pip || exit 10
 # reboot kernel
 mkinitcpio -p linux
-# oh-my-zsh
+# manually update oh-my-zsh
 echo "omz update" || exit 11
-# sync
-reset
+# refresh
+reset 
 sync
