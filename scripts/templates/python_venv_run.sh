308011991fc6c4e707a3c9170ce2f54ecef5375f
rodfer0x80 <rodfer0x80@proton.me>
2023-11-20 11:08:37 +0000

    AI

--- scripts/templates/python_venv_run.sh
+++ scripts/templates/python_venv_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test -e "$PWD/venv" &&\
+test -e "$PWD/.venv" &&\
     python3 -m venv "$PWD/.venv" &&\
     "$PWD/.venv/bin/python" -m pip install --upgrade pip &&\
     "$PWD/.venv/bin/python" -m pip install -r "$PWD/requirements.txt"
