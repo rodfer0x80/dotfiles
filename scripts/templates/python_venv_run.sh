0056935b3938495bb3e566e921c8dbd24cc5662c
rodfer0x80 <rodfer0x80@proton.me>
2023-11-20 11:41:34 +0000

    somuchtyping

--- scripts/templates/python_venv_run.sh
+++ scripts/templates/python_venv_run.sh
@@ -2,5 +2,7 @@
 
 test -e "$PWD/.venv" ||\
     /usr/bin/python3 -m venv "$PWD/.venv" &&\
-    "$PWD/.venv/bin/python" -m pip install --upgrade pip
-"$PWD/.venv/bin/python" -m pip install -r "$PWD/requirements.txt"
+    "$PWD/.venv/bin/python" -m pip install --upgrade pip &&\
+    "$PWD/.venv/bin/python" -m pip install -r "$PWD/requirements.txt"
+
+"$PWD/.venv/bin/python" main.py
