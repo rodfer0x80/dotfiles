3e58cae8d33eca63e87f741cbf81e8192879876f
rodfer0x80 <rodrigolf080@gmail.com>
2023-12-17 21:43:12 +0000

    ||good enought nvim||

--- .config/nvim/lua/configs/custom.lua
+++ .config/nvim/lua/configs/custom.lua
@@ -15,82 +15,85 @@ endfunction
 nnoremap <C-X><C-T> :call Toggle_transparent_background()<CR>
 ]])
 
-
 -- Basic setup
 vim.cmd([[
+"" Lines
+syntax on
+set ruler
+""set number
+set nu
+set relativenumber
 "" Encoding
 set encoding=utf-8
 set fileencoding=utf-8
 set fileencodings=utf-8
 set ttyfast
-
-"" Tabs. May be overridden by autocmd rules
+"" It's 2 or nothing 
 set tabstop=2
-set softtabstop=0
+set softtabstop=2
 set shiftwidth=2
 set expandtab
-
-"" Enable hidden buffers
-set hidden
-
+set smartindent
+"" buffers
+""set hidden=true
+""let no_buffers_menu=1
 "" Searching
-set hlsearch
 set incsearch
-set ignorecase
-set smartcase
-
+""set ignorecase=true
+""set smartcase=true
+"" nix all the things
 set fileformats=unix,dos,mac
-
 if exists('$SHELL')
     set shell=$SHELL
 else
     set shell=/bin/sh
 endif
-
 " session management
 let g:session_directory = "~/.vim/session"
 let g:session_autoload = "no"
 let g:session_autosave = "no"
 let g:session_command_aliases = 1
-
-syntax on
-set ruler
-set number
-
-let no_buffers_menu=1
-
-" Better command line completion
+"" Better command line completion
 set wildmenu
-
-" mouse support
-set mouse=a
-
-set mousemodel=popup
-set t_Co=256
-set guioptions=egmrti
-set gfn=Monospace\ 12
-
+"" mouse support
+""set mouse=a
+""set mousemodel=popup
 "" Disable the blinking cursor.
 set gcr=a:blinkon0
-
-set scrolloff=3
-
-
+"" gay stuff
+""set t_Co=256
+""set guioptions=egmrti
+set gfn=Monospace\ 12
+"" 16 lines down up is nice
+set scrolloff=16
 "" Status bar
 set laststatus=2
-
 "" Use modeline overrides
-set modeline
-set modelines=10
-
+""set modeline
+""set modelines=10
+"" more gay stuff
 set title
 set titleold="Terminal"
 set titlestring=%F
-
 set statusline=%F%m%r%h%w%=(%{&ff}/%Y)\ (line\ %l\/%L,\ col\ %c)\
-
 " Search mappings: These will make it so that going to the next one in a
 " search will center on the line it's found in.
 nnoremap n nzzzv
 nnoremap N Nzzzv
+"" *sexy not gay
 ]])
+vim.g.mapleader = " "
+-- undos and backups
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- *sexy colours, not gay
+vim.opt.termguicolors = true
+
+-- my dog wrote this part not sure what it does
+vim.opt.updatetime = 42
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
